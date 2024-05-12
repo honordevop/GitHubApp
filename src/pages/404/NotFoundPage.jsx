d547f70a61e60f859a155ac381d406fdcf906358
Ogunlade Stephen O <ogunladestephen20@gmail.com>
2024-05-13 00:45:33 +0100

    Code: Implemented 404 error page

--- src/pages/404/NotFoundPage.jsx
+++ src/pages/404/NotFoundPage.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className="w-[100vw] h-[400vh] flex text-white">
+      <div className="max-w-screen-xl mx-auto px-4 flex items-center justify-start h-screen md:px-8">
+        <div className="max-w-lg mx-auto space-y-3 text-center">
+          <h3 className=" font-semibold">404 Error</h3>
+          <p className=" text-4xl font-semibold sm:text-5xl">Page not found</p>
+          <p className="">
+            Sorry, the page you are looking for could not be found or has been
+            removed.
+          </p>
+          <div className="flex flex-wrap items-center justify-center gap-3">
+            <a
+              href="/"
+              className="block py-2 px-4 text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg"
+            >
+              Go back
+            </a>
+            <a
+              href="tel:+2348164462713"
+              className="block py-2 px-4 text-gray-700 hover:bg-gray-50 font-medium duration-150 active:bg-gray-100 border rounded-lg"
+            >
+              Contact support
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
